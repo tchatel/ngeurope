8ddb1b04a709e7b17147de8db1202da8258a495c
Thierry Chatel <tchatel@gmail.com>
2014-10-16 22:12:41 +0200

    branche tmp-l

--- exo/app/js/autoform.js
+++ exo/app/js/autoform.js
@@ -3,55 +3,66 @@
 angular.module('autoform', ['ngMessages', 'ngAnimate'])
 
     .directive('autoform', function ($injector) {
-         return {
-             restrict: 'AE',
-             templateUrl: 'templates/autoform.html',
-             transclude: true,
-             scope: {
-                 model: '=',
-                 editable: '='
-             },
-             controller: function ($scope) {
-                 this.isEditable = function () {
-                     return $scope.editable;
-                 };
-                 function swap(array, i, j) {
-                     var elem = array[i];
-                     array[i] = array[j];
-                     array[j] = elem;
-                 }
-                 this.moveUp = function (fieldDesc) {
-                     var index = $scope.desc.fields.indexOf(fieldDesc);
-                     if (index > 0) {
-                         swap($scope.desc.fields, index, index - 1);
-                     }
-                 };
-                 this.moveDown = function (fieldDesc) {
-                     var index = $scope.desc.fields.indexOf(fieldDesc);
-                     if (index < $scope.desc.fields.length - 1) {
-                         swap($scope.desc.fields, index, index + 1);
-                     }
-                 };
-                 this.remove = function (fieldDesc) {
-                     var index = $scope.desc.fields.indexOf(fieldDesc);
-                     $scope.desc.fields.splice(index, 1);
-                 };
-             },
-             link: function (scope, element, attrs) {
-                 if (attrs.service) {
-                     var loader = $injector.get(attrs.service);
-                     loader().then(function (desc) {
-                         scope.desc = desc;
-                     });
-                 } else {
-                     // Allow usage without service name, taking the description from the 'desc' attribute
-                     // The $watch must watch in parent (non isolated) scope.
-                     scope.$parent.$watch('desc', function (desc) {
-                         scope.desc = desc;
-                     });
-                 }
-             }
-         };
+        return {
+            restrict: 'AE',
+            templateUrl: 'templates/autoform.html',
+            transclude: true,
+            scope: {
+                model: '=',
+                editable: '='
+            },
+            controller: function ($scope) {
+                this.isEditable = function () {
+                    return $scope.editable;
+                };
+                function swap(array, i, j) {
+                    var elem = array[i];
+                    array[i] = array[j];
+                    array[j] = elem;
+                }
+
+                this.moveUp = function (fieldDesc) {
+                    var index = $scope.desc.fields.indexOf(fieldDesc);
+                    if (index > 0) {
+                        swap($scope.desc.fields, index, index - 1);
+                    }
+                };
+                this.moveDown = function (fieldDesc) {
+                    var index = $scope.desc.fields.indexOf(fieldDesc);
+                    if (index < $scope.desc.fields.length - 1) {
+                        swap($scope.desc.fields, index, index + 1);
+                    }
+                };
+                this.remove = function (fieldDesc) {
+                    var index = $scope.desc.fields.indexOf(fieldDesc);
+                    $scope.desc.fields.splice(index, 1);
+                };
+                var editField = null;
+                this.startEdit = function (fieldDesc) {
+                    editField = fieldDesc;
+                };
+                this.stopEdit = function (fieldDesc) {
+                    editField = null;
+                };
+                this.isEditing = function (fieldDesc) {
+                    return editField == fieldDesc;
+                };
+            },
+            link: function (scope, element, attrs) {
+                if (attrs.service) {
+                    var loader = $injector.get(attrs.service);
+                    loader().then(function (desc) {
+                        scope.desc = desc;
+                    });
+                } else {
+                    // Allow usage without service name, taking the description from the 'desc' attribute
+                    // The $watch must watch in parent (non isolated) scope.
+                    scope.$parent.$watch('desc', function (desc) {
+                        scope.desc = desc;
+                    });
+                }
+            }
+        };
     })
 
     .directive('autoformfield', function () {
@@ -75,6 +86,15 @@ angular.module('autoform', ['ngMessages', 'ngAnimate'])
                 };
                 scope.remove = function () {
                     ctrl.remove(scope.desc);
+                };
+                scope.startEdit = function () {
+                    ctrl.startEdit(scope.desc);
+                };
+                scope.stopEdit = function () {
+                    ctrl.stopEdit(scope.desc);
+                };
+                scope.isEditing = function () {
+                    return ctrl.isEditing(scope.desc);
                 };
             }
         };
