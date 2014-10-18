031dd4e7b2d193f5dc5c37f7cf8ac691da9cccf4
Thierry Chatel <tchatel@gmail.com>
2014-10-18 10:11:25 +0200

    branche tmp-o

--- exo/app/js/autoform.js
+++ exo/app/js/autoform.js
@@ -113,3 +113,35 @@ angular.module('autoform', ['ngMessages', 'ngAnimate'])
             }
         };
     })
+
+    // Directive pour saisir dans un champ texte un contenu JSON, utilisée pour définir la liste des options
+    // d'une liste déroulante (en mode édition du formulaire).
+    .directive('json', function () {
+        return {
+            restrict: 'A',
+            priority: 100,
+            scope: false,
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModelCtrl) {
+                // En version 1.3, la validation est séparée du $parser
+                ngModelCtrl.$validators['json'] = function (viewValue) {
+                    try {
+                        angular.fromJson(viewValue);
+                        return true;
+                    } catch (e) {
+                        return false;
+                    }
+                };
+                ngModelCtrl.$parsers.unshift(function (viewValue) {
+                    try {
+                        return angular.fromJson(viewValue);
+                    } catch (e) {
+                        return undefined;
+                    }
+                });
+                ngModelCtrl.$formatters.push(function (modelValue) {
+                    return angular.toJson(modelValue);
+                });
+            }
+        };
+    })
