3fc488cc98369de95bf06f6a15c9016d730fcc32
Thierry Chatel <tchatel@gmail.com>
2014-10-19 17:49:19 +0200

    mise au propre des branches

---
+++ app/js/autoform_test.js
@@ -0,0 +1,81 @@
+describe("directives", function () {
+
+    var descFieldText = {"property": "p1", "label": "label1", "type": "text", "constraints": {}};
+    var descFieldCheckbox = {"property": "p2", "label": "label2", "type": "checkbox", "constraints": {} };
+    var descFieldSelect = {"property": "p3", "label": "label3", "type": "select", "list": {"A": "aaa", "B": "bbb"}};
+    var descFieldEmail = {"property": "p4", "label": "label4", "type": "email", "constraints": {"required": true}};
+    var descForm = {
+        "fields": [descFieldText, descFieldCheckbox, descFieldSelect, descFieldEmail]
+    };
+
+    var jq = angular.element;
+
+    beforeEach(module('templates'));
+    beforeEach(module('autoform'));
+
+    describe("autoform directive", function () {
+
+        it("should create a form row for each field", function () {
+            inject(function ($compile, $rootScope) {
+                var template = '<autoform desc="desc" model="obj">Entête</autoform>';
+                var scope = $rootScope.$new();
+                scope.desc = descForm;
+                var autoform = $compile(template)(scope); // Compiles the template and links it to the scope
+                scope.$digest(); // Needed to update the directives templates
+                expect(autoform.find('autoformfield').length).toEqual(4);
+            });
+        });
+
+    });
+
+    describe("autoformfield directive", function () {
+        var scope, html;
+        beforeEach(function () {
+            inject(function ($compile, $rootScope) {
+                scope = $rootScope.$new();
+                html = $compile('<autoformfield desc="desc" model="obj"></autoform>')(scope);
+            });
+        });
+
+        it ("should generate a text input field", function () {
+            inject(function ($compile, $rootScope) {
+                scope.desc = descFieldText;
+                scope.$digest(); // Needed to update the directive template
+                expect(html.find('input').attr('ng-model')).toEqual('model[desc.property]');
+            });
+        });
+
+        it ("should generate a checkbox", function () {
+            inject(function ($compile, $rootScope) {
+                scope.desc = descFieldCheckbox;
+                scope.$digest(); // Needed to update the directive template
+                expect(html.find('input').attr('type')).toEqual('checkbox');
+            });
+        });
+
+        it ("should generate an email input field", function () {
+            inject(function ($compile, $rootScope) {
+                scope.desc = descFieldEmail;
+                scope.$digest(); // Needed to update the directive template
+                expect(html.find('input').attr('type')).toEqual('email');
+            });
+        });
+
+        it ("should generate a checkbox", function () {
+            inject(function ($compile, $rootScope) {
+                scope.desc = descFieldSelect;
+                scope.$digest(); // Needed to update the directive template
+                var options = html.find('select').find('option');
+                expect(options.length).toEqual(3);
+                var optionA = jq(options[1]);
+                var optionB = jq(options[2]);
+                expect(optionA.attr('value')).toEqual('A');
+                expect(optionA.text()).toEqual('aaa');
+                expect(optionB.attr('value')).toEqual('B');
+                expect(optionB.text()).toEqual('bbb');
+            });
+        });
+
+    });
+
+});
